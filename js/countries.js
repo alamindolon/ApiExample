c56931238c17250369fb3d6822edf5af4e98f0bf
Al Amin Sarker Dolon <80084331+alamindolon@users.noreply.github.com>
2023-03-21 16:40:24 +0600

    design part done

--- js/countries.js
+++ js/countries.js
@@ -15,17 +15,41 @@ const displayShowCountry = countries =>
 
         const countryDiv = document.createElement('div');
         countryDiv.classList.add('country');
-        console.log(country);
+       
 
         countryDiv.innerHTML = `
         
         <h3>Country Name: ${country.name.common} </h3>
         <p> Capital: ${country.capital ? country.capital[0] : 'No capital'} </p>
 
+        <button onclick = "displayDatails('${country.cca2}')">Details</button>
+
         `;
         divContainer.appendChild(countryDiv);
         
     });
+}
+
+const displayDatails = (code) =>
+{
+    const url = `https://restcountries.com/v3.1/alpha/${code}`
+    
+    // console.log(url);
+
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayDatailsShow(data[0]))
+}
+
+const displayDatailsShow =country =>
+{
+    const displayDetailsID =  document.getElementById('Country_DetailsID');
+    displayDetailsID.innerHTML = `
+    
+    <h2> Details: ${country.name.common}</h2>
+    <img src = '${country.flags.png}'>
+    `;
+
 }
 
 loadCountries();
