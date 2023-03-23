3f23b565bd1061b94236944721716c1fa6f47250
Al Amin Sarker Dolon <80084331+alamindolon@users.noreply.github.com>
2023-03-23 13:07:57 +0600

    async done

--- async.js
+++ async.js
@@ -2,9 +2,16 @@ const loadUserAsync = async() =>
 {
 
     const url = `https://randomuser.me/api/?gender=female`;
-    const res = await fetch(url);
-    const data = await res.json();
-    displayUser(data.results[0])
+    try
+    {
+            const res = await fetch(url);
+        const data = await res.json();
+        displayUser(data.results[0]);
+    }
+    catch (error)
+    {
+        console.log(error);
+    }
 } 
 
 const displayUser = user =>
